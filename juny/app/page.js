1751ba9bf24112e8d939bb10f419d200c4fa06f4
mchuang413 <lxsquidofficial@gmail.com>
2024-07-19 20:46:43 -0700

    Update page.js

--- juny/app/page.js
+++ juny/app/page.js
@@ -1,23 +1,10 @@
 "use client";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Overview from './learn/page';
-import Unit1 from './learn/units/unit1/page';
-import Unit2 from './learn/units/unit2';
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex">
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/learn" element={<Overview />} />
-            <Route path="/learn/unit1" element={<Unit1 />} />
-            <Route path="/learn/unit2" element={<Unit2 />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <>
+    </>
   );
 };
 
