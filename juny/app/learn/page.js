7107936228dc4f57dc89977d6dc42b7907a52818
mchuang413 <lxsquidofficial@gmail.com>
2024-07-29 19:40:00 -0700

    lock on the left of learn button

--- juny/app/learn/page.js
+++ juny/app/learn/page.js
@@ -338,7 +338,7 @@ const Page = () => {
                 <div className={`button w-40 h-16 ${userLevel >= unit.requiredLevel ? 'bg-blue-500' : 'bg-gray-500'} rounded-lg cursor-pointer select-none
                   active:translate-y-2 active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
                   active:border-b-[0px] transition-all duration-150 ${userLevel >= unit.requiredLevel ? '[box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841] border-b-[1px] border-blue-400' : '[box-shadow:0_10px_0_0_#888888,0_15px_0_0_#888888] border-b-[1px] border-gray-400'}`}>
-                  <span className='flex flex-col justify-center items-center h-full text-white font-bold text-lg '>
+                  <span className='flex flex-col justify-center items-center h-full text-white font-bold text-lg'>
                     {userLevel < unit.requiredLevel && <img src="/lock.png" alt="locked" className="h-8 w-8 mr-2" />}
                     Learn
                   </span>
