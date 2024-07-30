cd02961892fbb9c55a02a3d526ceb8447c2f3696
mchuang413 <lxsquidofficial@gmail.com>
2024-07-30 09:20:38 -0700

    Update page.js

--- juny/app/learn/page.js
+++ juny/app/learn/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 const Page = () => {
   const router = useRouter();
