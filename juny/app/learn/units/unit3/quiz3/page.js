e4751ef9b5c420f70cb517248a8971b19523d1a0
David Ryu <163350341+DavidRyuP5@users.noreply.github.com>
2024-07-29 19:03:37 -0700

    fix error

--- juny/app/learn/units/unit3/quiz3/page.js
+++ juny/app/learn/units/unit3/quiz3/page.js
@@ -8,7 +8,7 @@ const Page = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [aiFeedback, setAiFeedback] = useState("");
-  const numSteps = 5;
+  const numSteps = 4;
   const isPremium = true; // Change this to dynamically check for premium status
 
   const questions = [
